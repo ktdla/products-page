dd58879a72aac2b45808ff1393c07c28058b2508
Katherine De La Cruz <katherine.delacruz8@gmail.com>
2015-11-12 16:40:05 -0500

    Add fixed attribute to header

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,7 @@
+$(window).scroll(function(){
+  var sticky = $('.sticky'),
+      scroll = $(window).scrollTop();
+
+  if (scroll >= 30) sticky.addClass('fixed');
+  else sticky.removeClass('fixed');
+});
