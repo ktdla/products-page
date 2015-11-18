3271c68c167f203673b4349115d4fbeb9225bcca
Katherine De La Cruz <katherine.delacruz8@gmail.com>
2015-11-18 17:46:40 -0500

    Add video popup

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -13,4 +13,17 @@ $(document).ready(function () {
     }
   })
  })
+
+
+ //popup
+ $('.popup-vimeo').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false
+  });
+
 });
