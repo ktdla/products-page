605f9048141e559bf812d1b6c9bc03f1236d52cd
Katherine De La Cruz <katherine.delacruz8@gmail.com>
2015-11-19 17:47:28 -0500

    Add mailchimp-api to integration with mailchimp

--- app/controllers/subscribe_controller.rb
+++ app/controllers/subscribe_controller.rb
@@ -1,8 +1,9 @@
 class SubscribeController < ApplicationController
+  include Refinery::I18n
   def create
     logger.info "suscripcion #{params.inspect}"
     user = user_params(params)
-    MailchimpWrapper.subscribe(user)
+    MailchimpWrapper.subscribe(user, params[:group_name], params[:group])
     render 'refinery/pages/gracias'
   rescue Mailchimp::Error => e
     logger.error "ERROR mailchimp #{e.message} #{params.inspect}"
@@ -14,7 +15,9 @@ class SubscribeController < ApplicationController
     user = {
       email: params[:email].try(:downcase),
       first_name: params[:first_name].try(:titleize),
-      last_name: params[:last_name].try(:titleize)
+      last_name: params[:last_name].try(:titleize),
+      city: params[:city].try(:titleize),
+      comment: params[:comment].try(:titleize)
     }
     user
   end
--- lib/mailchimp_wrapper.rb
+++ lib/mailchimp_wrapper.rb
@@ -1,17 +1,21 @@
 module MailchimpWrapper
-  def self.subscribe(user)
+  def self.subscribe(user, group_name = nil, group = nil)
     mailchimp = Mailchimp::API.new(ENV['MAILCHIMP_API_KEY'])
-    merge_vars = merge_vars_hash(user)
+    merge_vars = merge_vars_hash(user, group_name, group)
     mailchimp.lists.subscribe(
       ENV['MAILCHIMP_LIST_ID'],
       { email: user[:email] },
       merge_vars)
   end
 
-  def self.merge_vars_hash(user)
+  def self.merge_vars_hash(user, group_name, group)
     merge_vars = {}
     merge_vars[:fname] = user[:first_name] unless user[:first_name].blank?
     merge_vars[:lname] = user[:last_name] unless user[:last_name].blank?
+    merge_vars[:city] = user[:city] unless user[:city].blank?
+    merge_vars[:comment] = user[:comment] unless user[:comment].blank?
+    groupings = { 0 => { name: group_name, groups: [group] } } if group_name && group
+    merge_vars[:groupings] = groupings
     merge_vars
   end
 end
