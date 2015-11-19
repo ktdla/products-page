ebaaec1754cebeceee53428111c9be70c414a01f
Moisés Ñañez <moisesnandres@gmail.com>
2015-11-19 13:04:49 -0500

    Filter categories

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,28 +1,35 @@
 $(document).ready(function () {
 
- $(".filter-toggle").click(function(){
+  $(".filter-toggle").click(function(){
          $(".active-filter").slideToggle();
      });
 
- //slides
- $(function() {
+  //slides
+  $(function() {
   $(".slides").slidesjs({
     height: 100,
     play: {
       auto: true
     }
   });
- });
+  });
 
- //filter posts in home
+  //filter categories from home
+  var url = window.location.href
+  var arr = url.split("#");
+  var category = arr[1].toLowerCase();
+  $(".products article").hide();
+  $("."+category).show();
+
+  //filter posts in home
   var elements = $('.news .post');
   $('.news .post').detach();
   elements.appendTo(".news");
   $('.news .post').hide();
   $('.news .post').slice(0,3).show();
 
- //popup
- $('.popup-vimeo').magnificPopup({
+  //popup
+  $('.popup-vimeo').magnificPopup({
     disableOn: 700,
     type: 'iframe',
     mainClass: 'mfp-fade',
